941bb2564176e38528397bb0b64584b8a6b1b40b
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-04-14 02:23:07 +0200

    :rocket: add authentication layout

---
+++ app/layouts/authentication.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Layout from "~/components/composing/Layout.vue";
+import { ArrowLeft } from "@lucide/vue";
+</script>
+
+<template>
+  <Layout name="authentication">
+    <header class="fixed top-0 inset-x-0 h-16 flex items-center gap-2 px-6">
+      <UiButton
+        size="icon-sm"
+        variant="ghost"
+        as-child
+      >
+        <NuxtLink to="/">
+          <ArrowLeft />
+        </NuxtLink>
+      </UiButton>
+      <p class="font-serif leading-none">
+        Aurora Lens
+      </p>
+    </header>
+
+    <main>
+      <NuxtPage />
+    </main>
+  </Layout>
+</template>
