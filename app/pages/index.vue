49132ecee561e6c64a4fe7b4bcbd39eeaaafd4b2
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-04-14 02:22:07 +0200

    :rocket: add default login/register link

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,6 +4,24 @@ import Page from "~/components/composing/Page.vue";
 
 <template>
   <Page>
-    home
+    <div class="flex items-center gap-1.5">
+      <UiButton
+        variant="ghost"
+        size="sm"
+        as-child
+      >
+        <NuxtLink to="/auth/register">
+          {{ $t("navigation.register") }}
+        </NuxtLink>
+      </UiButton>
+      <UiButton
+        size="sm"
+        as-child
+      >
+        <NuxtLink to="/auth/login">
+          {{ $t("navigation.login") }}
+        </NuxtLink>
+      </UiButton>
+    </div>
   </Page>
 </template>
