eb43e88dfeda7780a55ccdc8e3eeab6e2e3e8d29
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-04-14 02:23:18 +0200

    :rocket: setup login and register forms

---
+++ app/pages/auth/login.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Page from "~/components/composing/Page.vue";
+import LoginForm from "~/components/auth/LoginForm.vue";
+
+definePageMeta({
+  layout: "authentication",
+});
+</script>
+
+<template>
+  <Page
+    name="auth.login"
+    class="min-h-dvh grid place-items-center"
+  >
+    <LoginForm />
+  </Page>
+</template>
---
+++ app/pages/auth/register.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Page from "~/components/composing/Page.vue";
+import RegisterForm from "~/components/auth/RegisterForm.vue";
+
+definePageMeta({
+  layout: "authentication",
+});
+</script>
+
+<template>
+  <Page
+    name="auth.register"
+    class="min-h-dvh grid place-items-center"
+  >
+    <RegisterForm />
+  </Page>
+</template>
