8c3291df88174f27f1705a9db0387eedc4e30d8f
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-04-20 00:45:55 +0200

    :rocket: add exported password reset confirmed email template

--- emails/templates/index.ts
+++ emails/templates/index.ts
@@ -2,3 +2,4 @@ export type { EmailTemplate } from "./types";
 export { emailVerificationTemplate } from "./auth/email-verification";
 export { emailVerifiedTemplate } from "./auth/email-verified";
 export { passwordResetTemplate } from "./auth/password-reset";
+export { passwordResetConfirmedTemplate } from "./auth/password-reset-confirmed";
