6529601d0b191fbfa9d6ae06d52fe8f64e57077c
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-04-20 00:11:00 +0200

    :rocket: add base password reset pages

---
+++ app/pages/auth/forgot-password.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Page from "~/components/composing/Page.vue";
+</script>
+
+<template>
+  <Page name="auth.forgot-password">
+    forgot password
+  </Page>
+</template>
---
+++ app/pages/auth/reset-password.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Page from "~/components/composing/Page.vue";
+</script>
+
+<template>
+  <Page name="auth.reset-password">
+    reset password
+  </Page>
+</template>
